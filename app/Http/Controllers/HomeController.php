584a9ef2f799f8aa7e77bee7911892c5551fab8e
Adjie Projects <adjie.projects@gmail.com>
2020-09-07 17:32:46 +0700

    Trying role & permission by spatie

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class HomeController extends Controller
 {
@@ -23,6 +26,21 @@ class HomeController extends Controller
      */
     public function index()
     {
+        // $role = Role::create(['name' => 'writer']);
+        // $permission  = Permission::create(['name' => 'edit articles']);
+
+        // $role->givePermissionTo($permission);
+        // $permission->assignRole($role);
+
+        // $role = Role::create(['name' => 'super-admin']);
+        $user = Auth::user();
+
+        // $user->assignRole('super-admin');
+
+        // $user->givePermissionTo('edit articles');
+
+        // dd($user->hasPermissionTo('edit articles'));
+
         return view('home');
     }
 }
